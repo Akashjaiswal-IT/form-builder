12fdb625ab879141cff93daede15b39da0b9f84e
Akash <akashjaiswal190505@gmail.com>
2026-06-18 04:00:44 +0530

    chore: update imagekit

--- packages/services/clients/imagekit.ts
+++ packages/services/clients/imagekit.ts
@@ -1,18 +1,30 @@
 import ImageKit from "imagekit";
 import { env } from "../env";
 
-export const imageKitClient = new ImageKit({
-  publicKey: env.IMAGEKIT_PUBLIC_KEY ?? "",
-  privateKey: env.IMAGEKIT_PRIVATE_KEY ?? "",
-  urlEndpoint: env.IMAGEKIT_URL_ENDPOINT ?? "",
-});
+function createImageKitClient(): ImageKit | null {
+  if (env.IMAGEKIT_PUBLIC_KEY && env.IMAGEKIT_PRIVATE_KEY && env.IMAGEKIT_URL_ENDPOINT) {
+    return new ImageKit({
+      publicKey: env.IMAGEKIT_PUBLIC_KEY,
+      privateKey: env.IMAGEKIT_PRIVATE_KEY,
+      urlEndpoint: env.IMAGEKIT_URL_ENDPOINT,
+    });
+  }
+  return null;
+}
+
+const _client = createImageKitClient();
+
+export const imageKitClient = _client!;
 
 class ImageKitService {
   public getUploadAuthenticationParameters() {
+    if (!_client) {
+      throw new Error("ImageKit is not configured.");
+    }
     return {
-      ...imageKitClient.getAuthenticationParameters(),
-      publicKey: env.IMAGEKIT_PUBLIC_KEY ?? "",
-      urlEndpoint: env.IMAGEKIT_URL_ENDPOINT ?? "",
+      ..._client.getAuthenticationParameters(),
+      publicKey: env.IMAGEKIT_PUBLIC_KEY!,
+      urlEndpoint: env.IMAGEKIT_URL_ENDPOINT!,
     };
   }
 }
